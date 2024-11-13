857620bda7d3aa8b558cc679dcce592cea9947cb
Thiago Tula <gabytula08@gmail.com>
2024-11-13 20:25:00 -0300

    tercer comit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const conectarDb = async ()=>{
     try {
-        await mongoose.connect(process.env.BD)
+        await mongoose.connect('mongodb://thiago:14042004@ac-ge0zp8i-shard-00-00.yzcca3i.mongodb.net:27017,ac-ge0zp8i-shard-00-01.yzcca3i.mongodb.net:27017,ac-ge0zp8i-shard-00-02.yzcca3i.mongodb.net:27017/nba?ssl=true&replicaSet=atlas-opqq8l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
         console.log('Conectado a mongo exitosamente')
     } catch (error) {
         console.log(error);
